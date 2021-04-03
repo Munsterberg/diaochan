0ee453dd23c9a7aeb1b274008b5ead21829dbd27
munsterberg <jetrussell93@gmail.com>
2021-04-03 15:08:34 -0400

    enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
       forbidNonWhitelisted: true,
-    })
+    }),
   );
   await app.listen(3000);
 }
