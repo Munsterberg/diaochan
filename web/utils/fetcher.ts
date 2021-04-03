876ce9a610174ebd32446b71af38aa15a8d8b826
munsterberg <jetrussell93@gmail.com>
2021-04-03 14:19:12 -0400

    create custom fetcher wrapper

--- web/utils/fetcher.ts
+++ web/utils/fetcher.ts
@@ -1,1 +1,45 @@
-export const fetcher = (...args) => fetch(...args).then(res => res.json());
+const localStorageKey = '__diaochan_token__';
+
+// TODO: improve this lazy boi
+type Config = {
+  method: string
+  headers: any;
+  body?: any;
+}
+
+export function client(endpoint, { body, method, ...customConfig}) {
+  const headers = {'content-type': 'application/json'};
+
+  const config: Config = {
+    method: method ? method : 'GET',
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers
+    }
+  }
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  return window.fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
+    .then(async response => {
+      if (response.status === 401) {
+        logout();
+        return;
+      }
+
+      if (response.ok) {
+        return await response.json();
+      } else {
+        const errorMessage = await response.text();
+        return Promise.reject(new Error(errorMessage));
+      }
+    });
+}
+
+function logout()  {
+  window.localStorage.removeItem(localStorageKey);
+}
+
